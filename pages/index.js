75859e49c817b52dfca1f516086dba40b59b9d7d
axelobscura <axosar@gmail.com>
2020-10-07 11:46:54 -0500

    Titulos y fechas

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,15 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          SARCOPENIA
-        </h1>
+        <h1 className={styles.title}>1<sup>er</sup> Simposio de Sarcopenia y Fragilidad</h1>
+        <hr/>
+        <h2 style={{ textAlign: 'center', fontWeight: 'bold'}}>22 y 23 de octubre, 2020</h2>
+        <h3 style={{ textAlign: 'center', fontWeight: '100'}}>COSTO: $500.00<br/>CONSTANCIA CON VALOR CURRICULAR</h3>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
